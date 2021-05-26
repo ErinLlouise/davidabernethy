f017e38c606aaad5c4a4508c5409316d39dc9cd0
ErinLlouise <erinllouise3@gmail.com>
2021-05-27 00:01:23 +1200

    remove excess dependencies

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import {getGreeting} from '../apiClient'
 
-const App = () => {
-
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-      })
-  }, [count])
-
+function App () {
   return (
     <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+      <h1>David's Site</h1>
     </>
   )
 }
