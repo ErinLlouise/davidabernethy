726fb157ac62014a2faeafa27ec6f6986cb741d0
ErinLlouise <erinllouise3@gmail.com>
2021-08-20 17:21:28 +1200

    add grid to invisi card

--- client/components/IqBook.jsx
+++ client/components/IqBook.jsx
@@ -6,7 +6,7 @@ function IqBook () {
       <div>
         <h1 className='flex justify-center font-hero-font font-serif text-7xl pt-20'>IQ and the Y2K</h1>
         <div className='grid justify-center grid-flow-col grid-cols-12 grid-rows-12 gap-4x'>
-          <p className='col-start-4 col-span-6 bg-transparent mt-16 mb-10'>
+          <p className='col-start-4 col-span-6 bg-transparent mt-10 mb-10'>
             IQ was a rev from Sirius C Major. In his search for a star-translayer
             and some Y2K to power his spaceship, he ended up on Earth. There he met
             university students, George, Charley and Steve, and eccentric homeless
@@ -17,11 +17,23 @@ function IqBook () {
           <div className='col-start-4 col-span-6'>
             <div className='flex'>
               <figure>
-                <img className='w-96 h-auto pb-4'src='/images/IQ.jpg'></img>
+                <img className='w-80 h-auto pb-4'src='/images/IQ.jpg'></img>
                 <figcaption className='text-xs'>ISBN 978-0-473-55188-9</figcaption>
               </figure>
+              <div>
+                <p className='pl-4'>
+                  IQ and the Y2K, available in:
+                  <br></br>
+                  Paperback (203 x 127 x 12.5mm HWD)
+                  <br></br>
+                  Kindle ebook
+                  <br></br>
+                </p>
+              </div>
+              <div>
+                <a className='m-auto pt-10'href='https://www.amazon.com/IQ-Y2K-David-Abernethy/dp/0473551888'>Amazon</a>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
