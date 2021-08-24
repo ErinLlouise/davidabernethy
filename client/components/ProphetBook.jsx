e4595c1c3874f2ed96e72272726ba0ce8d5d97b6
ErinLlouise <erinllouise3@gmail.com>
2021-08-24 12:38:18 +1200

    organise jsx prophet image side contant to own component

--- client/components/ProphetBook.jsx
+++ client/components/ProphetBook.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProphetSideContent from './ProphetSideContent'
 
 function ProphetBook () {
   return (
@@ -22,49 +23,7 @@ function ProphetBook () {
             </p>
           </div>
           <div className='col-start-3 col-span-8'>
-            <div className='flex'>
-              <figure>
-                <img className='w-96 h-auto pb-4'src='/images/prophet.jpg'></img>
-                <figcaption className='text-xs pb-20'>ISBN 978-1-512-19832-4</figcaption>
-              </figure>
-              <div className='pl-10'>
-                <p>
-                  Sequential Circuits went on to create innovative
-                  <br></br>
-                  concepts and products such as programmable
-                  <br></br>
-                   effects, MIDI, multitimbrality, high quality
-                  <br></br>
-                   sampling, workstation and MPC systems and many
-                  <br></br>
-                  more. Today, the Prophet-5 is very much sought-after
-                  <br></br>
-                  as one of those truly iconic classic musical instruments.
-                  <br></br>
-                </p>
-                <p className='pt-6'>
-                Featuring many exclusive and highly entertaining and
-                  <br></br>
-                informative stories from ex-Sequential staff, music
-                  <br></br>
-                 industry moguls, and famous keyboard players.
-                </p>
-                <p className='pt-6 mb-4'>
-                  <em>
-                    IQ and the Y2K, available in:
-                    <br></br>
-                    <br></br>
-                    Paperback (203 x 127 x 12.5mm HWD)
-                    <br></br>
-                    Kindle ebook
-                    <br></br>
-                  </em>
-                </p>
-                <a className='m-auto pt-10'href='https://www.amazon.com/IQ-Y2K-David-Abernethy/dp/0473551888'>Amazon</a>
-              </div>
-              <div>
-              </div>
-            </div>
+            <ProphetSideContent />
           </div>
         </div>
       </div>
---
+++ client/components/ProphetSideContent.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function ProphetSideContent () {
+  return (
+    <>
+      <div className='flex'>
+        <figure>
+          <img className='w-96 h-auto pb-4'src='/images/prophet.jpg'></img>
+          <figcaption className='text-xs pb-20'>ISBN 978-1-512-19832-4</figcaption>
+        </figure>
+        <div className='pl-10'>
+          <p>
+            Sequential Circuits went on to create innovative
+            <br></br>
+            concepts and products such as programmable
+            <br></br>
+            effects, MIDI, multitimbrality, high quality
+            <br></br>
+            sampling, workstation and MPC systems and many
+            <br></br>
+            more. Today, the Prophet-5 is very much sought-after
+            <br></br>
+            as one of those truly iconic classic musical instruments.
+            <br></br>
+          </p>
+          <p className='pt-6'>
+            Featuring many exclusive and highly entertaining and
+            <br></br>
+            informative stories from ex-Sequential staff, music
+            <br></br>
+            industry moguls, and famous keyboard players.
+          </p>
+          <p className='pt-6 mb-4'>
+            <em>
+                IQ and the Y2K, available in:
+              <br></br>
+              <br></br>
+                    Paperback (203 x 127 x 12.5mm HWD)
+              <br></br>
+                Kindle ebook
+              <br></br>
+            </em>
+          </p>
+          <p>
+            <em>
+                Buy on <a className='m-auto pt-10 underline'href='https://www.amazon.com/IQ-Y2K-David-Abernethy/dp/0473551888'>Amazon</a> herea
+            </em>
+          </p>
+        </div>
+        <div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProphetSideContent
