27527ec1b70e6ec06afdd2be133b208aece059ce
ErinLlouise <erinllouise3@gmail.com>
2021-08-26 13:20:52 +1200

    wip fix carousel

--- client/components/CarouselData.js
+++ client/components/CarouselData.js
@@ -1,21 +1,20 @@
-// import ImageData01 from '../../server/public/images/ImgSlider/slider01.JPG'
 // import ImageData02 from '../../server/public/images/ImgSlider/slider02.JPG'
 // import ImageData03 from '../../server/public/images/ImgSlider/slider03.JPG'
 
-// export const images = [
-//   {
-//     title: 'First Image',
-//     subtitle: 'this is the first image',
-//     image: ImageData01
-//   },
-//   {
-//     title: 'Second Image',
-//     subtitle: 'this is the second image',
-//     image: ImageData02
-//   },
-//   {
-//     title: 'Third Image',
-//     subtitle: 'this is the third image',
-//     image: ImageData03
-//   }
-// ]
+export const images = [
+  {
+    title: 'First Image',
+    subtitle: 'this is the first image',
+    image: '/images/ImgSlider/slider01.JPG'
+  }
+  // {
+  //   title: 'Second Image',
+  //   subtitle: 'this is the second image',
+  //   image: ImageData02
+  // },
+  // {
+  //   title: 'Third Image',
+  //   subtitle: 'this is the third image',
+  //   image: ImageData03
+  // }
+]
--- client/components/Deck.jsx
+++ client/components/Deck.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { images } from './CarouselData'
+import { images } from './CarouselData'
 
 function Deck () {
   return (
     <>
       <div className=''>
         <h1>This works</h1>
-        {/* <img src={images[0].image}></img> */}
+        <img src={images[0].image}></img>
       </div>
     </>
   )
