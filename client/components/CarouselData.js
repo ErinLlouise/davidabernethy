dcf286d03c3443e30ae6e284d65f2ba86b520e77
ErinLlouise <erinllouise3@gmail.com>
2021-08-26 15:36:10 +1200

    change carousel data titles

--- client/components/CarouselData.js
+++ client/components/CarouselData.js
@@ -1,54 +1,54 @@
 export const images = [
   {
-    title: 'First Image',
+    title: 'A home made synthesizer using an arduino uno and paper clips',
     imageUrl: '/images/ImgSlider/slider01.JPG'
   },
   {
-    title: 'Second Image',
+    title: 'minimoog synthesizer',
     imageUrl: '/images/ImgSlider/slider02.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A and his first book',
     imageUrl: 'images/ImgSlider/slider03.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'Sequential Circuits',
     imageUrl: 'images/ImgSlider/slider04.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A',
     imageUrl: 'images/ImgSlider/slider05.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider06.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider07.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'Yamaha',
     imageUrl: 'images/ImgSlider/slider08.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider09.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider10.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider11.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider12.JPG'
   },
   {
-    title: 'Third Image',
+    title: 'David A studio',
     imageUrl: 'images/ImgSlider/slider13.JPG'
   }
 ]
