2f1d07cb79bce871a6a10e82724b3f66804018b4
ErinLlouise <erinllouise3@gmail.com>
2021-08-27 12:11:35 +1200

    style image carousel

--- client/components/Deck.jsx
+++ client/components/Deck.jsx
@@ -7,22 +7,22 @@ function Deck () {
     <>
       <div className='flex justify-center'>
         <div className='flex m-20'>
-          <div className='m-10 align-middle'>
-            <button onClick={() => {
+          <div className='m-auto pr-6 align-middle cursor-pointer'
+            onClick={() => {
               currentImage > 0 && setCurrentImage(currentImage - 1)
-            }}
-            >back</button>
+            }}>
+            <img src='/images/backarrow.png'></img>
           </div>
           <img
             className='rounded shadow-lg h-96'
             src={images[currentImage].imageUrl}
           >
           </img>
-          <div className='m-10 align-middle'>
-            <button onClick={() => {
+          <div className='m-auto pl-6 align-middle cursor-pointer'
+            onClick={() => {
               currentImage < images.length - 1 && setCurrentImage(currentImage + 1)
-            }}
-            >forward</button>
+            }}>
+            <img src='/images/forwardarrow.png'></img>
           </div>
         </div>
       </div>
