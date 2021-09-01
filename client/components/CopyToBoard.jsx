9c3a76b852a162c2f66eb47cc000231011447040
ErinLlouise <erinllouise3@gmail.com>
2021-09-02 11:54:37 +1200

    oncopy on mail image working

--- client/components/CopyToBoard.jsx
+++ client/components/CopyToBoard.jsx
@@ -2,37 +2,18 @@ import React, { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 function CopyToBoard () {
-  const [value, setValue] = useState('')
   const [copied, setCopied] = useState(false)
   return (
     <>
       <div>
-        <input
-          className='bg-yellow-100'
-          value={value}
-          onChange={({ target: { value } }) => {
-            setValue(value)
-            setCopied(false)
-          }}
-        />
-        {/* <CopyToClipboard text={value}
-          onCopy={() => {
-            setCopied(true)
-          }
-          }>
-          <span>Copy to clipboard with span</span>
-        </CopyToClipboard> */}
         <CopyToClipboard
-          text={value}
-          onCopy={() => {
-            setCopied(true)
-          }}
-        >
-          <button
-            className='bg-yellow'
-          >Copy to clipboard</button>
+          text={'erinabernethydev@gmail.com'}
+          onCopy={() => setCopied(true)}>
+          <img
+            className='cursor-pointer transform hover:scale-105 duration-500 h-40 w-auto'
+            src='/images/mail.png'
+          />
         </CopyToClipboard>
-
         {copied ? <span style={{ color: 'red' }}>Copied</span> : null}
       </div>
     </>
